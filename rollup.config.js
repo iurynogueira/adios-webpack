26b590250cdd9c72487ba820e60e7667d4a5df8e
Iury Nogueira <iunog@hotmail.com>
2023-03-21 14:32:16 -0300

    fix with babel

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,25 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import { readdirSync } from 'fs';
+import path from 'path';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
+import replace from 'rollup-plugin-replace';
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 
-const pkg = require('./package.json');
+
+const EXTENSIONS = ['.ts', '.tsx', '.js', '.jsx', '.json'];
+const commonPlugins = () => [
+  external({
+    includeDependencies: true,
+  }),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+];
 
 export default [
   {
@@ -29,6 +45,6 @@ export default [
   {
     input: 'dist/esm/index.d.ts',
     output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-    plugins: [dts()],
+    plugins: [dts(), ...commonPlugins()],
   },
 ];
